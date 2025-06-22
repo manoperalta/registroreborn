0c9f4c9c965ad5085a0aeaa7032fd436fc1ae2c7
manoperalta <manoperalta@gmail.com>
2025-06-22 00:22:28 -0300

    templates base

--- app/settings.py
+++ app/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['app/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

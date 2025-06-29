111f9d31bb97bf4bc7c33d27f92cc11c794b3a7f
manoperalta <manoperalta@gmail.com>
2025-06-29 08:17:41 -0300

    alteracoes base template

--- certidao/signals.py
+++ certidao/signals.py
@@ -8,14 +8,13 @@ from django.dispatch import receiver
 
 @receiver(post_save, sender=CertidaoNascimento)
 def certidaonascimento_post_save(sender, instance, **kwargs):
-    # Renderiza HTML com os dados da certidão
+    
     html_string = render_to_string('certidao/certidao_nascimento.html', {'certidao': instance})
     
-    # Define caminho para salvar o PDF
+    
     output_dir = os.path.join(settings.MEDIA_ROOT, 'certidoes')
     os.makedirs(output_dir, exist_ok=True)
     output_path = os.path.join(output_dir, f'certidao_{instance.id}.pdf')
     
-    # Gera o PDF com WeasyPrint
     HTML(string=html_string).write_pdf(output_path)
 

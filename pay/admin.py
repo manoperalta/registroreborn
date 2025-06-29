175b61be9760047a54d16990bf8242272a716f11
manoperalta <manoperalta@gmail.com>
2025-06-29 13:48:46 -0300

    Area de Admin Payment

--- pay/admin.py
+++ pay/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Payment
 
-# Register your models here.
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('user','product_name', 'price', 'status', 'mercadopago_payment_id', 'created_at', 'updated_at')
